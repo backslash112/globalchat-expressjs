2fc97f5a5d85997c07adf7e968d2a7a2adb34847
backslash112 <yangcun@live.com>
2018-06-26 14:57:31 -0500

    fix a bug for mongoose.methods.generateToken(), https://stackoverflow.com/a/36795534/2195426

---
+++ test/models/user.js
@@ -0,0 +1,10 @@
+var user = require('../../models/user');
+var chai = require('chai');
+var expect = chai.expect;
+var should = chai.should();
+
+describe('models/user.js', () => {
+  before(done => {});
+  after(done => {});
+  it('should ', (done) => {});
+});
